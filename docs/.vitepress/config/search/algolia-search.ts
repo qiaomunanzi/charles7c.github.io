8ca004100404b2ad3659d2a3eab652a46a11d441
Mr.joe <qiaomunanzi@163.com>
2024-08-01 11:17:31 +0800

    Update algolia-search.ts

--- docs/.vitepress/config/search/algolia-search.ts
+++ docs/.vitepress/config/search/algolia-search.ts
@@ -1,9 +1,9 @@
 import type { AlgoliaSearchOptions } from 'vitepress';
 
 export const algoliaSearchOptions: AlgoliaSearchOptions = {
-  appId: 'DBZ0G9HBUY',
-  apiKey: '00cef480a543003d05d9808110ea5f65',
-  indexName: 'charles7c',
+  appId: 'GZWLIEMA04',
+  apiKey: '4fe3df150c8c5a1c988459bfb1e3ff35',
+  indexName: 'qxs-top',
   locales: {
     root: {
       placeholder: '搜索文档',
