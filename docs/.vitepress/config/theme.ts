61d69f8a22c86c19009b13638500e4bdf0250d76
Mr.joe <qiaomunanzi@163.com>
2024-08-04 12:35:50 +0800

    Update theme.ts

--- docs/.vitepress/config/theme.ts
+++ docs/.vitepress/config/theme.ts
@@ -29,8 +29,8 @@ export const themeConfig: DefaultTheme.Config = {
   },
   // 搜索配置（二选一）
   search: {
-    provider: 'algolia',
-    options: algoliaSearchOptions,
+    provider: 'local',
+    options: localSearchOptions,
     // 本地离线搜索
     // provider: 'local',
     // options: localSearchOptions
@@ -73,19 +73,5 @@ export const themeConfig: DefaultTheme.Config = {
     icpRecordCode: '冀ICP备2024005888号-1', // ICP备案号
     publicSecurityRecordCode: '冀公网安备12011202000688号', // 联网备案号
     copyright: `Copyright © ${new Date().getFullYear()} 乔先森` // 版权信息
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3"/>
-  },
-  {
-    "url": "http://www.qxs-docs.top/sitemap.xml",
-    "status": "SKIPPED",
-    "statusReason": "http_redirect_invalid",
-    "duration": 1632,
-    "http": {
-      "code": 308,
-      "contentType": null,
-      "contentLength": 14
-    },
-    "records": [],
-    "links": []
   }
 }
