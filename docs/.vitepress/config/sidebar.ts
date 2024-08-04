ddb752f1115d66a2cd31d8d5a54b550a1fad925b
Mr.joe <qiaomunanzi@163.com>
2024-08-04 21:53:34 +0800

    update

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -11,7 +11,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
   '/categories/fragments/': getItemsByDate("categories/fragments"),
   '/categories/solutions/': getItemsByDate("categories/solutions"),
   '/categories/tools/': getItemsByDate("categories/tools"),
-
+  '/categories/favorites/': getItemsByDate("categories/favorites"),
   '/courses/java/': getItems("courses/java"),
   '/courses/mysql/': getItems("courses/mysql"),
   '/courses/mybatis/': getItems("courses/mybatis"),
