6511fc69a8235bd94c0be8804ee7a636ef896047
Mr.joe <qiaomunanzi@163.com>
2024-08-04 16:24:10 +0800

    Update nav.ts

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -5,7 +5,7 @@ export const nav: DefaultTheme.Config['nav'] = [
   {
     text: '我的分类',
     items: [
-      { text: 'All in one', link: '/categories/all-in-one/index', activeMatch: '/categories/all-in-one/' }
+      { text: 'All in one', link: '/categories/all-in-one/index', activeMatch: '/categories/all-in-one/' },
       { text: 'Bug万象集', link: '/categories/issues/index', activeMatch: '/categories/issues/' },
       { text: '"杂碎"逆袭史', link: '/categories/fragments/index', activeMatch: '/categories/fragments/' },
       { text: '工具四海谈', link: '/categories/tools/index', activeMatch: '/categories/tools/' },
